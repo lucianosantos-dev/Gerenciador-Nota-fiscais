7fbf2ed6d046f3ee6aefd43174b1d2c1047d6df4
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-04-01 22:08:57 -0300

    Refactor (controller): update NotaFiscalController

--- src/main/java/com/example/Gerenciador_Nota_fiscal/controller/NotaFiscalController.java
+++ src/main/java/com/example/Gerenciador_Nota_fiscal/controller/NotaFiscalController.java
@@ -1,74 +1,84 @@
 package com.example.Gerenciador_Nota_fiscal.controller;
 
-import com.example.Gerenciador_Nota_fiscal.entities.NotaFiscal;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Gerenciador_Nota_fiscal.dtos.NotaFiscalDto;
+import com.example.Gerenciador_Nota_fiscal.mappers.NotaFiscalMapper;
+import com.example.Gerenciador_Nota_fiscal.services.NotaFiscalService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 public class NotaFiscalController {
 
-    private final OrganizadorDeNotas organizador;
+    private final NotaFiscalService service;
+    private final NotaFiscalMapper mapper;
 
-    @Autowired
-    public NotaFiscalController(OrganizadorDeNotas organizador) {
-        this.organizador = organizador;
+    public NotaFiscalController(NotaFiscalService service, NotaFiscalMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
     }
 
-    @GetMapping("/")
-    public String mostrarPaginaInicial(Model model) {
-        List<NotaFiscal> notas = organizador.listarTodas();
-        model.addAttribute("notas", notas);
 
-        List<NotaFiscal> notasAVencer = organizador.listarNotasProximasDoVencimento();
-        model.addAttribute("notasDeAlerta", notasAVencer);
-        return "index";
-    }
-
-    @GetMapping("/notas/novo")
-    public String mostrarFormularioDeAdicao(Model model) {
-        model.addAttribute("notaFiscal", new NotaFiscal());
-        return "formulario-nota";
-    }
-
-    @PostMapping("/notas/salvar")
-    public String salvarNota(@ModelAttribute NotaFiscal notaFiscal) {
-        if (notaFiscal.getNumero() == null || notaFiscal.getNumero().isEmpty()) {
-            notaFiscal.setNumero(UUID.randomUUID().toString());
+    @PostMapping("/notas")
+    public ResponseEntity<NotaFiscalDto> save(@RequestBody NotaFiscalDto notaFiscalDto) {
+        NotaFiscalDto dto = service.save(notaFiscalDto);
+        if (dto != null) {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+            return ResponseEntity.created(uri).body(dto);
         }
-        organizador.adicionar(notaFiscal);
-        return "redirect:/";
+        return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/notas/editar/{numero}")
-    public String mostrarFormularioDeEdicao(@PathVariable String numero, Model model) {
-        Optional<NotaFiscal> resultadoBusca = organizador.buscarPorNumero(numero);
-        if (resultadoBusca.isPresent()) {
-            model.addAttribute("notaFiscal", resultadoBusca.get());
-        } else {
-            return "redirect:/";
+    @PutMapping("/notas/{numero}")
+    public ResponseEntity<NotaFiscalDto> update(@PathVariable String numero, @RequestBody NotaFiscalDto notaFiscalDto) {
+        NotaFiscalDto notaAtualizada = service.update(numero, notaFiscalDto);
+        if (notaAtualizada != null) {
+            return ResponseEntity.ok(notaAtualizada);
         }
-        return "formulario-nota";
+        return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/notas/excluir/{numero}")
-    public String excluirNota(@PathVariable String numero) {
-        organizador.excluirPorNumero(numero);
-        return "redirect:/";
+    @GetMapping("/notas")
+    public ResponseEntity<List<NotaFiscalDto>> findAll() {
+        return ResponseEntity.ok(service.findAll());
     }
 
-    @PostMapping("/notas/pagar/{numero}")
-    public String marcarNotaPaga(@PathVariable String numero) {
-        organizador.marcarComoPaga(numero);
-        return "redirect:/";
+    @GetMapping("/notas/{numero}")
+    public ResponseEntity<Optional<NotaFiscalDto>> findByNumero(@PathVariable String numero) {
+        return ResponseEntity.ok(service.findByNumero(numero));
+    }
+
+    @PatchMapping("/notas/{numero}/pagamento")
+    public ResponseEntity<Void> marcarComoPaga(@PathVariable String numero) {
+        boolean atualizado = service.marcarComoPaga(numero);
+        if (atualizado) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/notas/{numero}")
+    public ResponseEntity<Void> deleteByNumero(@PathVariable String numero) {
+        Optional<NotaFiscalDto> opt = service.findByNumero(numero);
+        if (opt.isPresent()) {
+            service.deleteByNumero(numero);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/notas/vencendo")
+    public ResponseEntity<List<NotaFiscalDto>> listarProximasVencimento() {
+        List<NotaFiscalDto> list = service.listarNotasProximasDoVencimento();
+        if (list.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(list);
     }
 
 }
